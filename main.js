9c270ad700f4ac4ddeb26cfefc458884356760d2
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2023-10-17 11:04:31 -0300

    save

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+//Criando/Declarando a função tocaSomPom
 function tocaSomPom(){
     document.querySelector("#som_tecla_pom").play();
 }
+//Chamando/Invocando a função tocaSomPom pelo clique no botão Pom
 document.querySelector(".tecla_pom").onclick = tocaSomPom;
 
-function tocaSomClap(){
-    document.querySelector("#som_tecla_clap").play();
-}
-document.querySelector(".tecla_clap").onclick = tocaSomClap;
+//Criando referência constante ListaDeTeclas buscando todos os itens pela classe .tecla
+const ListaDeTeclas = document.querySelectorAll(".tecla");
+
+//Chamando a função tocaSomPom a partir do item "0", 1° botão
+ListaDeTeclas[0].onclick = tocaSomPom;
