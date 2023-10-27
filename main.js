fe5ce8721dee2b6f13a4678796c9b78a4ff12925
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2023-10-26 22:31:35 -0300

    save

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-//Criando/Declarando a função tocaSom com parâmetro idElementoAudio
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+//Criando/declarando a função tocaSom
+function tocaSom(IDaudio){
+    document.querySelector(IDaudio).play();
 }
 
-//Criando referência constante ListaDeTeclas buscando todos os itens pela classe .tecla
-const ListaDeTeclas = document.querySelectorAll(".tecla");
+//Criando referência constante ListaDeTeclas buscando todos
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //invocando a função tocaSomPom a partir do item 0, 1º botão
 listaDeTeclas [0].onclick = tocaSom;
 
-//Criando referência variável "contador" iniciando = 0
-let contador = 0;
+//criando referência variável "contador" iniciando = 0
+let contador = 0; 
 
-//Estrutura de repetição "while" significa "enquanto algo acontecer"
-while(contador < ListaDeTeclas.length ){
-    const efeito = ListaDeTeclas [contador].classList [1];
-    const idAudio = "#som_"+ efeito;
-    ListaDeTeclas[contador].onclick = function (){
-         tocaSom(idAudio);
+//estrutura de repetição while = enquanto
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const IDaudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(IDaudio);
     }
     contador = contador + 1;
+
     console.log(contador);
 }
